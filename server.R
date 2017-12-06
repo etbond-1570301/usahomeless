ab15a50399b902d72482b6bda83b862bf6235e58
erika <erikataylorbond@gmail.com>
2017-12-05 21:53:18 -0800

    initial push by erika

---
+++ server.R
@@ -0,0 +1,109 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+
+#Set working directory:
+#setwd("~/Desktop/info201/usahomelessness") 
+
+#Read in data.
+homeless.df <- read.csv('./data/homeless.csv')
+
+##WA Dataframes.
+#Filter data for Washington only.
+wa.plot.data <- filter(homeless.df, State == 'WA', Measures == "Total Homeless") %>%
+  select(State, Count, Year, Measures, CoC.Name)
+#Add new column that eliminates commas from Count for graphic plotting.
+wa.plot.data$Count2 = as.numeric(gsub("\\,", "", wa.plot.data$Count))
+#Create data frame for necessary Seattle plot data.
+sea.plot.data <- filter(homeless.df, State == "WA", CoC.Name == "Seattle/King County CoC", Measures %in% c("Sheltered Homeless", "Unsheltered Homeless")) %>%
+  select(State, Count, Year, Measures, CoC.Name)
+#Remove commas for plotting.
+sea.plot.data$Count2 = as.numeric(gsub("\\,", "", sea.plot.data$Count))
+
+#######################################################################
+
+##Server
+
+shinyServer(function(input, output) {
+
+
+##Overview.
+  
+  
+##USA Homelessness. 
+  
+  
+##Washington.
+output$waPlot <- renderPlot({
+  
+  #Filter the dataset based on manufacturer input.
+   if(input$Year == "2007") {
+   plot.data = wa.plot.data %>% filter(Year == "1/1/2007")
+  } else if (input$Year == "2008") {
+   plot.data = wa.plot.data %>% filter(Year == "1/1/2008")
+   } else if (input$Year == "2009") {
+    plot.data = wa.plot.data %>% filter(Year == "1/1/2009")
+    } else if (input$Year == "2010") {
+     plot.data = wa.plot.data %>% filter(Year == "1/1/2010")
+    } else if (input$Year == "2011") {
+    plot.data = wa.plot.data %>% filter(Year == "1/1/2011")
+    } else if (input$Year == "2012") {
+     plot.data = wa.plot.data %>% filter(Year == "1/1/2012")
+    } else if (input$Year == "2013") {
+     plot.data = wa.plot.data %>% filter(Year == "1/1/2013")
+     } else if (input$Year == "2014") {
+      plot.data = wa.plot.data %>% filter(Year == "1/1/2014")
+   } else if (input$Year == "2015") {
+    plot.data = wa.plot.data %>% filter(Year == "1/1/2015")
+   } else if (input$Year == "2016") {
+      plot.data = wa.plot.data %>% filter(Year == "1/1/2016")
+   } else {
+      plot.data = wa.plot.data
+   }
+  
+  #Using the defined values, construct a WA barplot using ggplot2
+  ggplot(plot.data, aes(x=Year, y=Count2, fill = CoC.Name)) + 
+  geom_bar(stat="identity", aes(color= CoC.Name), position=position_dodge()) +
+    theme(axis.text.x = element_text(size  = 10, angle = 45, hjust = 1, vjust = 1)) +
+    theme(axis.text.y = element_text(size = 10, hjust = 1, vjust = 1)) +
+    labs(title = "Annual Homeless Population By Washington County", x = "Year of Data Collection", y = "Homeless Individuals")
+})
+
+output$seaPlot <- renderPlot({
+  if(input$Year == "2007") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2007")
+  } else if (input$Year == "2008") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2008")
+  } else if (input$Year == "2009") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2009")
+  } else if (input$Year == "2010") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2010")
+  } else if (input$Year == "2011") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2011")
+  } else if (input$Year == "2012") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2012")
+  } else if (input$Year == "2013") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2013")
+  } else if (input$Year == "2014") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2014")
+  } else if (input$Year == "2015") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2015")
+  } else if (input$Year == "2016") {
+    plot.data = sea.plot.data %>% filter(Year == "1/1/2016")
+  } else {
+    plot.data = sea.plot.data
+  }
+  
+  
+    ggplot(data=sea.plot.data, aes(Year, factor(Count2), fill=Measures)) +
+    geom_bar(stat="identity", aes(color= Measures), position=position_dodge()) +
+    theme_minimal() + 
+    theme(axis.text.x = element_text(size  = 10, angle = 45, hjust = 1, vjust = 1)) +
+    theme(axis.text.y = element_text(size = 7, hjust = 1, vjust = 1)) +
+    labs(title = "Annual Homeless Breakdown (King County)", x = "Year of Data Collection", y = "Sheltered/Unsheltered Homeless Individuals")
+  
+  
+})
+
+})
